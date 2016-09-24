27af48e4f5854c14e30572abe3190b9558ab9cb5
Elina <elinagoldin@gmail.com>
2016-09-23 21:19:31 -0700

    Initial setup with base files and directories - including helper SCSS files, header & footer files, and setup for compiling JS & SCSS

---
+++ include/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Site Title</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="theme-color" content="#000000">
+
+	<link rel="stylesheet" href="styles/main.css">
+	
+	<script src="scripts/main-min.js"></script>
+
+</head>
+<body>

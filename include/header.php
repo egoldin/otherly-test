45ef617074fa0baa9848e5dc43ad387877e11951
Elina <elinagoldin@gmail.com>
2016-09-24 08:46:52 -0700

    Added PHP variables to facilitate dynamic page titles and IDs

--- include/header.php
+++ include/header.php
@@ -3,13 +3,24 @@
 
 <head>
 	<meta charset="UTF-8">
-	<title>Site Title</title>
+	<title><?php echo $pageTitle; ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="theme-color" content="#000000">
-
-	<link rel="stylesheet" href="styles/main.css">
 	
+	<link rel="stylesheet" href="styles/main.css">
+
 	<script src="scripts/main-min.js"></script>
+	<?php 
+		// Use in case of external file specific to a page
+		/*if ($pageTitle == 'Page Title Here') {
+			echo '<script src="scripts/example.js"></script>';
+		}*/
+	?>
 
 </head>
-<body>
+
+<body id="<?php echo $pageID; ?>">
+
+	<header>
+		
+	</header>
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<?php 
+/**
+ * Page Variables
+ */
+
+$pageTitle = 'Page Title Here';
+$pageID = str_replace( ' ', '-', strtolower( $pagetitle ) ); // convert page title to lowercase and replace spaces with dashes
+
+?>
+
 <?php include('include/header.php') ?>
 
-
+<section>
+	
+</section>
 
 <?php include('include/footer.php') ?>
